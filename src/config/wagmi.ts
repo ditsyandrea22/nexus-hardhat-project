0ad3286e549b47f98f5122410fb5eac5d465ab9b
ditsy <34813777+ditsyandrea22@users.noreply.github.com>
2025-07-08 15:51:13 +0700

    Fix console errors and warnings

--- src/config/wagmi.ts
+++ src/config/wagmi.ts
@@ -3,7 +3,8 @@ import { nexusTestnet } from './chains'
 
 export const wagmiConfig = getDefaultConfig({
   appName: 'Nexus ERC20 Deployer',
-  projectId: '34548f090395c08d157465907480cc5d', // Get from https://cloud.walletconnect.com
+  projectId: '34548f090395c08d157465907480cc5d',
   chains: [nexusTestnet],
   ssr: false,
+  multiInjectedProviderDiscovery: false,
 })
